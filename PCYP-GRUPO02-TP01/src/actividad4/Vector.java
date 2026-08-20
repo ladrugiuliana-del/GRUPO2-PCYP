070d5a1fff7cef50fb018faabc5bb685c1294786
Giuliana Ladrú <ladrugiuliana@gmail.com>
2026-08-20 17:03:38 -0300

    Lectura y muestra de array.
    Lee por teclado 10 números enteros distintos a 0 (positivos y
    negativos) y los almacena en un array.
    
    Muestra el vector final.
    
    !!Falta agregar la condición que filtre de -100 a 100.
    
    Se agrega la clase "operaciones" vacía.

---
+++ PCYP-GRUPO02-TP01/src/actividad4/Vector.java
@@ -0,0 +1,43 @@
+package actividad4;
+
+import java.util.Scanner;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int[] numeros = new int[10];
+		int candidato;
+		boolean valido = false;
+	
+		for (int i = 0; i < numeros.length; i++) {
+			
+			System.out.print("Ingrese el valor N°" + (i+1) + ": ");
+			
+			do {
+			
+				candidato = scanner.nextInt();
+				
+				if (candidato != 0) {
+					numeros[i]= candidato;
+					valido = true;
+				}else {
+					System.out.println("El valor deber ser distinto de 0. Reintente.");
+					System.out.print("Ingrese el valor N°" + (i+1) + ": ");
+				}
+				
+			}while(!valido);
+			valido= false;
+			
+		}
+		
+			//Muestra del vector
+			System.out.print("VECTOR: ");
+			for (int i = 0; i < numeros.length; i++) {
+			    System.out.print(numeros[i] + " ");
+			}
+			scanner.close();
+	
+
+}}
