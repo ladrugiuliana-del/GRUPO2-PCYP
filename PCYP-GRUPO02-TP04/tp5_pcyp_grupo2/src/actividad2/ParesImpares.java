cdcbc5e516b77af9430eedc6139883d8bba7c68e
Giuliana Ladrú <ladrugiuliana@gmail.com>
2026-09-16 20:23:04 -0300

    Implementación Actividad 2
    Se implementa la lógica para imprimir pares e impares por consola de forma concurrente.

--- PCYP-GRUPO02-TP04/tp5_pcyp_grupo2/src/actividad2/ParesImpares.java
+++ PCYP-GRUPO02-TP04/tp5_pcyp_grupo2/src/actividad2/ParesImpares.java
@@ -1,5 +1,36 @@
 package actividad2;
 
 public class ParesImpares {
+	public static void main(String[] args) {
 
+        // Hilo para la Tarea 1
+        Thread pares = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=1 ; i<=10 ; i++) {
+                	if(i % 2 == 0) {
+                		System.out.println(i);
+                	}
+                }
+                	
+            }
+        });
+
+        // Hilo para la Tarea 2
+        Thread impares = new Thread(new Runnable() {
+        	@Override
+            public void run() {
+                for(int i=1 ; i<=10 ; i++) {
+                	if(i % 2 != 0) {
+                		System.out.println(i);
+                	}
+                }
+                	
+            }
+        });
+
+        // Ejecución concurrente de ambos hilos
+        impares.start();
+        pares.start();
+    }
 }
